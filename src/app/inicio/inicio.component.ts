1b465f134bf248cdf9ec4e4db53324447cb12bdb
Guilherme <guilherme.silva307@fatec.sp.gov.br>
2021-07-06 12:21:04 -0300

    implementação dos gets e posts inicio

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Temas';
+import { InicioService } from '../service/inicio.service';
 
 @Component({
   selector: 'app-inicio',
@@ -11,21 +14,35 @@ export class InicioComponent implements OnInit {
 
   nome = environment.nome
 
+  postagens: Postagem[]
+
+  temas: Tema[]
+
+  postagem: Postagem = new Postagem()
+
+  temaEscolhido: number;
+
+  tema: Tema = new Tema()
+
   
 
   constructor(
-   private router: Router
+   private router: Router,
+   private inicioService: InicioService
   ) { }
 
   ngOnInit() {
 
-    if(environment.token == ''){
+ /*   if(environment.token == ''){
       alert('Sessão expirada, faça login novamente')
       this.router.navigate(['/entrar'])
 
 
-    }
+    }*/
 
+    this.findPostagensComuns()
+
+    this.getAllTemas()
   }
 
 
@@ -50,4 +67,54 @@ export class InicioComponent implements OnInit {
     return foto;
 
   }
+
+  findPostagensComuns(){
+    this.inicioService.getAllPostagensComuns().subscribe((resp: Postagem[])=>{
+      this.postagens = resp;
+
+      console.log(this.postagens)
+    })
+  }
+
+  tipoTema(event: any){
+
+    this.temaEscolhido = event.target.value
+    this.postagem.tema.id = this.temaEscolhido
+  
+    console.log(this.temaEscolhido)
+    }
+
+  PostPostagensComum(){
+
+      this.postagem.usuario.id = environment.id
+
+
+    this.inicioService.postPostagemComum(this.postagem).subscribe((resp: Postagem)=>{
+      this.postagem = resp;
+
+      
+
+
+      alert('Postagem realizada com sucesso')
+
+     this.router.navigate(['/inicio'])
+    })
+  }
+
+  getAllTemas(){
+    this.inicioService.getTemasComuns().subscribe((resp: Tema[])=>{
+      this.temas = resp
+    })
+  }
+
+  findByIdTema(){
+    this.inicioService.getByIdTema(this.temaEscolhido).subscribe((resp: Tema)=>{
+      this.tema = resp;
+    })
+  }
+
+
+
+
+
   }
---
+++ src/app/service/inicio.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Temas';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InicioService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  refreshToken(){
+    this.token = {
+      headers: new HttpHeaders().set('Authorization', environment.token)
+    }
+  }
+
+  getAllPostagensComuns(): Observable<Postagem[]>{
+    return this.http.get<Postagem[]>('https://redeingressa.herokuapp.com/postagens/comuns', this.token)
+  }
+
+  postPostagemComum(postagem: Postagem): Observable<Postagem>{
+    return this.http.post<Postagem>('https://redeingressa.herokuapp.com/postagens', postagem, this.token)
+  }
+
+  getTemasComuns(): Observable<Tema[]>{
+    return this.http.get<Tema[]>('https://redeingressa.herokuapp.com/tema/comuns', this.token)
+  }
+
+  getByIdTema(id: number): Observable<Tema> {
+    return this.http.get<Tema>(`https://redeingressa.herokuapp.com/tema/${id}`, this.token
+    );
+  }
+
+}
